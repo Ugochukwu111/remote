77cd3a285c9b12b686205d5010aa342a422a401e
Ugochukwu <pas111cal3@gmail.com>
2024-07-14 09:14:11 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+   let sideBarEl = document.querySelector('.side-bar')
+   sideBarEl.style.display = 'flex'
+}
+
+function closeSideBar(){
+     let sideBarEl = document.querySelector('.side-bar')
+   sideBarEl.style.display = 'none'
+}
